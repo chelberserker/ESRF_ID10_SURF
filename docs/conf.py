760352a7f5d262fa568aa3298d37e117f77ffd60
egorbersenev <bersenjow@gmail.com>
2026-01-23 17:47:57 +0100

    Added a Mythen calibration and updated usage_example.ipynb

--- docs/conf.py
+++ docs/conf.py
@@ -18,6 +18,7 @@ author = 'Egor A. Bersenev'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
+    'myst_parser',
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
